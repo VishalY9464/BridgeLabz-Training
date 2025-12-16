f1b0e391ccdbf80e73886544bab21056b5f2b8cf
VishalY9464 <yaddavvishal9464@gmail.com>
2025-12-16 17:03:13 +0530

    [Vishal Yadav] Added: Program of Area of circle

---
+++ gcr-codebase/program-prerequisite/AreaOfCircle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+    public static double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of the circle: ");
+        double radius = sc.nextDouble();
+        double area = calculateArea(radius);
+        System.out.println("Area of Circle: " + area);
+
+   
+    }
+}
