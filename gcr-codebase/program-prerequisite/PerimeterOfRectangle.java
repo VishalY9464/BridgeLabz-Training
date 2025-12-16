c622761629a0825fb6a7a8ef4614bdd2cc68827b
VishalY9464 <yaddavvishal9464@gmail.com>
2025-12-16 19:53:31 +0530

    [Vishal Yadav] Added: Perimeter Of Rectangle

--- gcr-codebase/program-prerequisite/PerimeterOfRectangle.java
+++ gcr-codebase/program-prerequisite/PerimeterOfRectangle.java
@@ -5,13 +5,14 @@ public class PerimeterOfRectangle {
         return 2 * (length + width);
     }
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter length: ");
         double length = sc.nextDouble();
         System.out.print("Enter width: ");
         double width = sc.nextDouble();
         double perimeter = calculatePerimeter(length, width);
-        System.out.println("Perimeter of Rectangle: " + perimeter);
+        System.out.println("Perimeter of Rectangle:" + perimeter);
 
     }
 }
