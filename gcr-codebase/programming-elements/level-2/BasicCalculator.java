f51f25cae8554fc7b7702f7bdc6502fe53aaee23
VishalY9464 <yaddavvishal9464@gmail.com>
2025-12-17 15:31:37 +0530

    [Vishal Yadav]: added BasicCalculator file

--- gcr-codebase/programming-elements/level-2/BasicCalculator.java
+++ gcr-codebase/programming-elements/level-2/BasicCalculator.java
@@ -22,6 +22,6 @@ public class BasicCalculator
         System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " 
                            + number1 + " and " + number2 + " is " 
                            + addition + ", " + subtraction + ", " 
-                           + multiplication + ", and " + division); //Display the result
+                           + multiplication + ", and " + division);//Display the result
     }
 }
