471b7d08a1d1f126eee055b8a078e22c3b6f4d6c
VishalY9464 <yaddavvishal9464@gmail.com>
2025-12-18 16:20:40 +0530

    [Vishal Yadav] : added countdown logic using for loop

---
+++ gcr-codebase/control-flow/Level-1/CountdownForLoop.java
@@ -0,0 +1,11 @@
+// Program to print countdown using for loop
+
+public class CountdownForLoop {
+    public static void main(String[] args) {
+
+        // Countdown from 10 to 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
