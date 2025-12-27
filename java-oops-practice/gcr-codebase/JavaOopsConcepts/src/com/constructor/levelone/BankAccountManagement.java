01dba888c939e5ee62d9bbe6224521af91503388
VishalY9464 <yaddavvishal9464@gmail.com>
2025-12-27 23:27:48 +0530

    [Vishal Yadav] : changes in bank account management program

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/levelone/BankAccountManagement.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/levelone/BankAccountManagement.java
@@ -1,5 +1,47 @@
 package com.constructor.levelone;
 
+// BankAccount class
+class BankAccount {
+
+    public int accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    // set balance
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // get balance
+    public double getBalance() {
+        return balance;
+    }
+}
+
+// Child class
+class SavingsAccount extends BankAccount {
+
+    SavingsAccount(int accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+    }
+
+    public void display() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+    }
+}
+
+// Main class
 public class BankAccountManagement {
 
+    public static void main(String[] args) {
+
+        SavingsAccount account = new SavingsAccount(123456, "Vishal");
+
+        account.display();
+
+        account.setBalance(50000);
+        System.out.println("Balance: " + account.getBalance());
+    }
 }
