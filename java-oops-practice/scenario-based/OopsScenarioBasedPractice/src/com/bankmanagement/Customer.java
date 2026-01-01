5b1eeef8feee071644c96c28d8549e61d197f83b
VishalY9464 <yaddavvishal9464@gmail.com>
2026-01-01 13:06:25 +0530

    [Vishal Yadav] : added customer class for bank system

--- java-oops-practice/scenario-based/OopsScenarioBasedPractice/src/com/bankmanagement/Customer.java
+++ java-oops-practice/scenario-based/OopsScenarioBasedPractice/src/com/bankmanagement/Customer.java
@@ -1,31 +1,30 @@
 package com.bankmanagement;
 
-
 class Customer {
 
-    // Name of the customer
+    // Customer name
     private String name;
 
-    // Account associated with the customer
+    // Account associated with customer
     Account account;
 
-    // Constructor to initialize customer details
+    // Constructor
     public Customer(String name, Account account) {
         this.name = name;
         this.account = account;
     }
 
-    // Method to deposit money
+    // Deposit money
     public void deposit(double amount) {
         account.depositMoney(amount);
     }
 
-    // Method to withdraw money
+    // Withdraw money
     public void withdraw(double amount) {
         account.withdrawMoney(amount);
     }
 
-    // Method to display account details
+    // Display account details
     public void accountDetail() {
         System.out.println("Account Holder Name: " + name);
         account.display();
