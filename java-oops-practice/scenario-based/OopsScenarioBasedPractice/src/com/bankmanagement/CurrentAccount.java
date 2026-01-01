3bdbae5c101a0e9ecae46207c4ced9a23cccb0ad
VishalY9464 <yaddavvishal9464@gmail.com>
2026-01-01 13:07:17 +0530

    [Vishal Yadav] : added current account class

---
+++ java-oops-practice/scenario-based/OopsScenarioBasedPractice/src/com/bankmanagement/CurrentAccount.java
@@ -0,0 +1,31 @@
+package com.bankmanagement;
+
+
+public class CurrentAccount extends Account {
+
+    // Overdraft limit allowed
+    private double overdraftLimit;
+
+    // Constructor
+    public CurrentAccount(String accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    // Overriding withdraw method to allow overdraft
+    @Override
+    public void withdrawMoney(double amount) {
+        if (amount <= balance + overdraftLimit) {
+            balance -= amount;
+        } else {
+            System.out.println("Overdraft limit exceeded!");
+        }
+    }
+
+    // Display account details
+    public void displayCurrentAccount() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+        System.out.println("Overdraft Limit: " + overdraftLimit);
+    }
+}
