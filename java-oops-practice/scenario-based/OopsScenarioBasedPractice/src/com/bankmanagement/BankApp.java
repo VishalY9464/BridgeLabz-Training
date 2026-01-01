6e97315e7b214af392e311b949885d826cb78612
VishalY9464 <yaddavvishal9464@gmail.com>
2026-01-01 13:06:20 +0530

    [Vishal Yadav] : added bank application main logic

--- java-oops-practice/scenario-based/OopsScenarioBasedPractice/src/com/bankmanagement/BankApp.java
+++ java-oops-practice/scenario-based/OopsScenarioBasedPractice/src/com/bankmanagement/BankApp.java
@@ -1,24 +1,34 @@
 package com.bankmanagement;
 
+/*
+ * Main class to test the Bank Management System
+ */
 public class BankApp {
 
     public static void main(String[] args) {
 
-        // Create a savings account object
-        SavingAccount sa = new SavingAccount(0.05, "98896273", 1000);
+        // ----- SAVING ACCOUNT -----
+        SavingAccount sa = new SavingAccount(0.05, "SA1001", 1000);
+        Customer customer1 = new Customer("Vishal Yadav", sa);
 
-        // Create customer and link account
-        Customer c1 = new Customer("Vishal Yadav", sa);
-
-        // Perform operations
-        c1.deposit(1000);
-        c1.withdraw(500);
-
-        // Calculate interest
+        customer1.deposit(1000);
+        customer1.withdraw(500);
         sa.calculateInterest();
 
-        // Display final account details
-        c1.accountDetail();
+        System.out.println("---- Saving Account Details ----");
+        customer1.accountDetail();
         sa.displayInterest();
+
+        System.out.println();
+
+        // ----- CURRENT ACCOUNT -----
+        CurrentAccount ca = new CurrentAccount("CA2001", 2000, 1000);
+        Customer customer2 = new Customer("Rahul Sharma", ca);
+
+        customer2.withdraw(2500); // allowed due to overdraft
+
+        System.out.println("---- Current Account Details ----");
+        customer2.accountDetail();
+        ca.displayCurrentAccount();
     }
 }
