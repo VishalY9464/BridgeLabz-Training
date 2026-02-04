2944cbbc35de3f27df833ae0282d96a811b07095
VishalY9464 <yaddavvishal9464@gmail.com>
2026-02-04 11:32:09 +0530

    [Vishal Yadav]:program for design student library management system

---
+++ java-design-pattern-practice/gcr-codebase/JavaDesignPatternConcepts/src/main/java/com/casestudy/studentlibrarymanagementsystem/Librarian.java
@@ -0,0 +1,12 @@
+package com.casestudy.studentlibrarymanagementsystem;
+public class Librarian extends User {
+
+    public Librarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public void getRole() {
+        System.out.println(name + " is Librarian (manages inventory)");
+    }
+}
